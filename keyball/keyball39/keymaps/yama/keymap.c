a29be1cda0ae2c1b748feea960bfe07e07a5d0e0
SoichiroYamane <soichiro.yamane.19970619@gmail.com>
2026-08-12 12:31:57 +0900

    fix: update yama mouse keycodes

--- keyball/keyball39/keymaps/yama/keymap.c
+++ keyball/keyball39/keymaps/yama/keymap.c
@@ -91,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Mouse Layer
   [5] = LAYOUT_universal(
-    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                            KC_MCTL  , XXXXXXX  , KC_BTN3  , XXXXXXX  , XXXXXXX,
-    KC_LSFT  ,LCMD(KC_Z),LCMD(KC_C),LCMD(KC_V),LCMD(KC_F),                            XXXXXXX  , KC_BTN1  , KC_BTN2  , MO(6)    , KC_ENT ,
-    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                            XXXXXXX  , KC_BTN4  , KC_BTN5  , XXXXXXX  , XXXXXXX,
+    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                            KC_MCTL  , XXXXXXX  , MS_BTN3  , XXXXXXX  , XXXXXXX,
+    KC_LSFT  ,LCMD(KC_Z),LCMD(KC_C),LCMD(KC_V),LCMD(KC_F),                            XXXXXXX  , MS_BTN1  , MS_BTN2  , MO(6)    , KC_ENT ,
+    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                            XXXXXXX  , MS_BTN4  , MS_BTN5  , XXXXXXX  , XXXXXXX,
     XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,      XXXXXXX  , TG(5)    , XXXXXXX  , XXXXXXX  , XXXXXXX  , KC_CAPS
   ),
 
@@ -112,4 +112,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,      XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX
   ),
 };
-
